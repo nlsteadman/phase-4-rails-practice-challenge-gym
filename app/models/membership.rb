7132363b4d0a18d34132b9581e6cbfef7990ed7a
Nicole Steadman <nsteadman@gmail.com>
2022-06-08 19:49:31 -0700

    commit tree fe21a02455f20b73446902194992e7762e64c639

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ApplicationRecord
     validates :gym_id, presence: true
     validates :client_id, presence: true
     validates :charge, presence: true
-    validates :client_id, uniqueness:true
+    validates :client, uniqueness: { scope: :gym }
     belongs_to :client
     belongs_to :gym
 end
